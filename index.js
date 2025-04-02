98f9f62a20f8c73f1a618ec920682a520df7d6ae
nagi <angus.5267@gmail.com>
2025-04-02 15:07:13 +0800

    v0.0.0

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.use(express.static('PlanET/dist'));
+
+const port = process.env.PORT || 8080 ;
+app.listen(port , () =>{
+    console.log(`Listening on port ${port}`);
+});
+
+app.get('/api/pirates/:id',(req,res)=>{
+    const id = req.params.id;
+    const pirate = getPirate(id);
+    if(!pirate){
+        res.status(404).send({error:`Pirate ${id} not found`});
+    }else{
+        res.send({data:pirate});
+    }
+});
+
+
+function getPirate(id){
+    const pirates =[
+        {id: 1 , name:`Klaus Stortebeker`,active:'1392-1401' , country:"Germany"},
+        {id: 2 , name:`Tuanku Abbas`,active:'to 1844' , country:"Germany"},
+        {id: 3 , name:`Ching Shih`,active:'18007-1810' , country:"Germany"}
+    ];
+    return pirates.find( p => p.id == id);
+}
